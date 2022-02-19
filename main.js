05d361307109330ed30b30451dd3ae335ff07525
Luc-Ky Khuu <khuu96@gmail.com>
2022-02-18 16:52:04 -0800

    added dom tree creation for table

--- main.js
+++ main.js
@@ -20,11 +20,24 @@ $week.addEventListener('click', function (event) {
   if (event.target.tagName !== 'BUTTON') {
     return;
   }
-
   var $h3Element = document.querySelector('h3');
   $h3Element.textContent = 'Scheduled Events for ' + event.target.textContent;
-  // var weekObj = {
-  //   id: data.mon[0].id,
-  // }
-
+  var $dataDay = event.target.getAttribute('data-day');
+  $tbody.innerHTML = '';
+  for (var i = 0; i < data[$dataDay].length; i++) {
+    makeTree(data[$dataDay][i]);
+  }
 });
+
+var $tbody = document.querySelector('tbody');
+
+function makeTree(schedule) {
+  var tr = document.createElement('tr');
+  var td1 = document.createElement('td');
+  td1.textContent = schedule.time;
+  var td2 = document.createElement('td');
+  td2.textContent = schedule.description;
+  tr.appendChild(td1);
+  tr.appendChild(td2);
+  $tbody.appendChild(tr);
+}
