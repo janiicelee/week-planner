4f19d8b66df6dd6fb2e04f572f74a6817b7546ea
Janice Lee <meekukin@gmail.com>
2022-02-18 16:27:59 -0800

    Created eventlistener for each button of days and created data model

---
+++ data.js
@@ -0,0 +1,28 @@
+/* exported data */
+
+var data = {
+  mon: [
+    {
+      id: 1,
+      time: 4,
+      description: 'eat'
+    },
+    {
+      id: 2,
+      time: 5,
+      description: 'homework'
+    }
+  ],
+  tue: [
+    {
+      id: 1,
+      time: 2,
+      description: 'dsf'
+    },
+    {
+      id: 2,
+      time: 3,
+      description: 'code'
+    }
+  ]
+};
--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+/* global data */
+/* exported data */
+
 var $overlay = document.querySelector('.overlay');
 var $addEntry = document.querySelector('#add-entry');
 
@@ -9,4 +12,19 @@ var $submit = document.querySelector('#submit');
 
 $submit.addEventListener('click', function (event) {
   $overlay.classList.add('hidden');
+});
+
+var $week = document.querySelector('#week');
+
+$week.addEventListener('click', function (event) {
+  if (event.target.tagName !== 'BUTTON') {
+    return;
+  }
+
+  var $h3Element = document.querySelector('h3');
+  $h3Element.textContent = 'Scheduled Events for ' + event.target.textContent;
+  // var weekObj = {
+  //   id: data.mon[0].id,
+  // }
+
 });
